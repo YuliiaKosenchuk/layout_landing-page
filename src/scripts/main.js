38ccf5da68307c45168537de1db11cc7030ad582
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-01-22 14:06:32 +0100

    add aside

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+const burger = document.querySelector('.icon--menu');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', (e) => {
+  menu.classList.add('active');
+});
+
+const closeBtn = document.querySelector('.icon--close');
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
