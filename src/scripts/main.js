104803ef822ad96e761e35f3675472d18b3f441a
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-02-12 16:19:55 +0100

    add adapt header and grid main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,24 @@
 'use strict';
+
 const burger = document.querySelector('.icon--menu');
 const menu = document.querySelector('.menu');
-
-burger.addEventListener('click', (e) => {
-  menu.classList.add('active');
-});
-
 const closeBtn = document.querySelector('.icon--close');
+const body = document.body;
+
+burger.addEventListener('click', () => {
+  menu.classList.add('active');
+  body.classList.add('no-scroll');
+});
 
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('active');
+  body.classList.remove('no-scroll');
+});
+
+const menuLinks = document.querySelectorAll('.menu__link');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+    body.classList.remove('no-scroll');
+  });
 });
